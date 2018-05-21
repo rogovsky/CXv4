195e8fa62051ae5dd96390860177ecdfa389e0d1
femanov <femanov@gmail.com>
2018-05-22 00:14:10 +0700

    make exports added

--- cx_build/make_all.sh
+++ cx_build/make_all.sh
@@ -4,5 +4,9 @@ set -e
 
 CXDIR=$HOME/cx
 
+# create directiries for exports
 make -C $CXDIR/4cx/src create-exports
+# make inplace
 make -C $CXDIR/4cx/src NOX=1
+# copy what just maked to exports
+make -C $CXDIR/4cx/src exports
